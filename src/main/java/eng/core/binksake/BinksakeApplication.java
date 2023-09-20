d9be4d6d3e444232cc4dd0601972354996f596fe
sm44832 <sm44832@zut.edu.pl>
2023-09-20 13:06:27 +0200

    BIN repo init and user data

---
+++ src/main/java/eng/core/binksake/BinksakeApplication.java
@@ -0,0 +1,13 @@
+package eng.core.binksake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinksakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinksakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eng/core/binksake/BinksakeApplicationTests.java
@@ -0,0 +1,13 @@
+package eng.core.binksake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinksakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
